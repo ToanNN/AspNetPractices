cc7ffbe3367a2ca938c7b8181fade594a0991abe
Toan Nguyen <toan.nguyen@qr.com.au>
2023-03-09 09:55:05 +1000

    Register chat hub

---
+++ SignalRWithBlazor/Server/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWithBlazor.Server.Hubs;
+
+public class ChatHub :Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
