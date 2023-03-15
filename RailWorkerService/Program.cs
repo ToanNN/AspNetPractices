d4d05e9103d3dc4548d4156a265862ef8539d4a3
Toan Nguyen <toan.nguyen@qr.com.au>
2023-03-15 12:40:25 +1000

    Add background worker service

---
+++ RailWorkerService/Program.cs
@@ -0,0 +1,10 @@
+using RailWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
